9218328830fbde16881adf7ac7050a5c76f3f956
Seb071201 <sebastien.micheli@yahoo.fr>
2021-12-17 09:07:19 +0100

    canvas

--- main.py
+++ main.py
@@ -9,9 +9,20 @@ from tkinter import Canvas, Tk, Label, Button, Entry, PhotoImage
 
 wd=Tk()
 wd.title("Space invaders")
-wd.geometry("500x300+400+200")
+wd.geometry("800x600+250+30")
+
+txt1=Label(wd, text="Score : ")
+txt2=Label(wd, text="Vies : ")
+
+can=Canvas(wd, width=550, height=550, bg="blue")
+#earth= PhotoImage(file="image/earth.jpg")
+#item=can.create_image(80,80, image=earth)
 
 butonQuitt= Button(wd, text="QUITTER", fg="red", command=wd.destroy)
-butonQuitt.pack()
+
+txt1.grid(row=1, column=1)
+txt2.grid(row=1, column=2)
+can.grid(row=2, column=1)
+butonQuitt.grid(row=2, column=2)
 
 wd.mainloop()
